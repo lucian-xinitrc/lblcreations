ddad71aaa43a336e8ccd458e8bf02fcf214ab26b
lucian-xinitrc <cusmirlucian@gmail.com>
2025-08-27 19:47:16 +0000

    New commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import { Navbar } from "../components/navbar.js";
 import { Header } from "../components/header.js";
-
+import { Benefits, About, Contact } from "../components/main.js";
 export default function Home() {
   return (
     <section className="bg-[#d9d9d9] h-screen">
       <Navbar />
       <Header />
+      <Benefits />
+      <About/>
+      <Contact />
     </section>
   );
 }
---
+++ src/components/main.js
@@ -0,0 +1,88 @@
+const Benefits = () => {
+	return (
+		<>
+			<section class="bg-white dark:bg-gray-900 py-40">
+			  <div class="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
+			      <div class="max-w-screen mb-8 lg:mb-16">
+			          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white text-center">Designed for business teams like yours</h2>
+			          <p class="text-gray-500 sm:text-xl dark:text-gray-400 text-center">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+			      </div>
+			      <div class="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
+			      	
+			          <div>
+			              <div class="flex justify-center items-center mb-4 w-10 h-10 w-full rounded-full bg-primary-100 lg:h-12 lg:w-full dark:bg-primary-900">
+			                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path></svg>
+			              </div>
+			              <h3 class="mb-2 text-xl font-bold dark:text-white text-center">Marketing</h3>
+			              <p class="text-gray-500 dark:text-gray-400 text-center ">Plan it, create it, launch it. Collaborate seamlessly with all  the organization and hit your marketing goals every month with our marketing plan.</p>
+			          </div>
+			          <div>
+			              <div class="flex justify-center items-center mb-4 w-full h-10 rounded-full bg-primary-100 lg:h-12 lg:w-full dark:bg-primary-900">
+			                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.394 2.08a1 1 0 00-.788 0l-7 3a1 1 0 000 1.84L5.25 8.051a.999.999 0 01.356-.257l4-1.714a1 1 0 11.788 1.838L7.667 9.088l1.94.831a1 1 0 00.787 0l7-3a1 1 0 000-1.838l-7-3zM3.31 9.397L5 10.12v4.102a8.969 8.969 0 00-1.05-.174 1 1 0 01-.89-.89 11.115 11.115 0 01.25-3.762zM9.3 16.573A9.026 9.026 0 007 14.935v-3.957l1.818.78a3 3 0 002.364 0l5.508-2.361a11.026 11.026 0 01.25 3.762 1 1 0 01-.89.89 8.968 8.968 0 00-5.35 2.524 1 1 0 01-1.4 0zM6 18a1 1 0 001-1v-2.065a8.935 8.935 0 00-2-.712V17a1 1 0 001 1z"></path></svg>
+			              </div>
+			              <h3 class="mb-2 text-xl font-bold dark:text-white text-center">Legal</h3>
+			              <p class="text-gray-500 dark:text-gray-400 text-center">Protect your organization, devices and stay compliant with our structured workflows and custom permissions made for you.</p>
+			          </div>
+			          <div>
+			              <div class="flex justify-center items-center mb-4 w-full h-10 rounded-full bg-primary-100 lg:h-12 lg:w-full dark:bg-primary-900">
+			                  <svg class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z" clip-rule="evenodd"></path><path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z"></path></svg>                    
+			              </div>
+			              <h3 class="mb-2 text-xl font-bold dark:text-white text-center">Business Automation</h3>
+			              <p class="text-gray-500 dark:text-gray-400 text-center">Auto-assign tasks, send Slack messages, and much more. Now power up with hundreds of new templates to help you get started.</p>
+			          </div>
+			          
+			      </div>
+			  </div>
+			</section>
+		</>
+	);
+}
+
+const About = () => {
+	return (
+		<>
+			<section class="bg-white dark:bg-[#d9d9d9] py-20 text-black">
+				<div class="max-w-screen mb-8 lg:mb-16">
+			        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black text-black text-center">About us</h2>
+			    </div>
+			    <div class="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+
+			        <img class="w-full dark:hidden" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg" alt="dashboard image" />
+			        <img class="w-full hidden dark:block" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup-dark.svg" alt="dashboard image" />
+			        <div class="mt-4 md:mt-0">
+			            <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-black text-black">Who we are, and what we do</h2>
+			            <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-black">We are a company that offers printing services with photo editing included</p>
+			        </div>
+			    </div>
+			</section>
+		</>
+	);
+}
+
+const Contact = () => {
+	return (
+		<section class="bg-white dark:bg-gray-900">
+		  <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+		      <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">Contact Us</h2>
+		      <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">Got a technical issue? Want to send feedback about a beta feature? Need details about our Business plan? Let us know.</p>
+		      <form action="#" class="space-y-8">
+		          <div>
+		              <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Your email</label>
+		              <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+		          </div>
+		          <div>
+		              <label for="subject" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Subject</label>
+		              <input type="text" id="subject" class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="Let us know how we can help you" requiredv />
+		          </div>
+		          <div class="sm:col-span-2">
+		              <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Your message</label>
+		              <textarea id="message" rows="6" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Leave a comment..."></textarea>
+		          </div>
+		          <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+		      </form>
+		  </div>
+		</section>
+	);
+}
+
+export { Benefits, About, Contact };
